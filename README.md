d401ef6e8921b230636279679f689598b6933e52
Maciek Stopa <maciej.stopa@techmo.pl>
2023-02-19 13:25:54 +0100

    Init pl_itn structure

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # pl_itn
+Inverse Text Normalization is an NLP task of changing the spoken form of a phrase to written form, for example:
+```
+one two three -> 1 2 3
+```
+
+`pl_itn` is an opensource Polish ITN Python library and REST API for practical applications.
+
+This project is an implementation of [NeMo Inverse Text Normalization](https://arxiv.org/abs/2104.05055) for Polish.
+
+## Table of contents
+
+[Setup](#setup)\
+[Usage](#usage)\
+[Documentation](#documentation)\
+[Contributing](#contributing)\
+[License](#License)
+
+## Setup
+```bash
+python3.8 -m venv .venv
+source .venv/bin/activate
+pip install wheel
+pip install .
+```
+
+## Usage
+
+## Documentation
+
+## Contributing
+
+## License
---
+++ pl_itn/src/normalize.py
@@ -0,0 +1,75 @@
+import logging
+from pathlib import Path
+import re
+
+from pl_itn.src.tag import Tagger
+from pl_itn.src.parse import TokenParser
+from pl_itn.src.permute import Permuter
+from pl_itn.src.verbalize import Verbalizer
+from pl_itn.src.restore_whitespace import WhitespaceRestorer, punctuation
+from pl_itn.src.restore_uppercase import UppercaseRestorer
+
+logger = logging.getLogger(__name__)
+
+
+class Normalizer:
+    def __init__(self,
+                 tagger_fst_path: Path,
+                 verbalizer_fst_path: Path,
+                 debug_mode: bool = False
+                 ):
+        # thread safe objects
+        self.tagger = Tagger(tagger_fst_path)
+        self.parser = TokenParser()
+        self.verbalizer = Verbalizer(verbalizer_fst_path)
+        self.debug_mode = debug_mode
+
+        # objects to initialize in self.normalize() per session due to race conditions:
+        # Permuter, UppercaseRestorer
+
+    def normalize(self, text: str) -> str:
+        logger.debug(f"input: {text}")
+
+        preprocessed_text = self.pre_process(text)
+
+        if len(preprocessed_text) == 0:
+            logger.info("Empty input string")
+            return text
+        else:
+            logger.debug(f"pre_process(): {preprocessed_text}")
+
+        try:
+            tagged_text = self.tagger.tag(preprocessed_text)
+            tokens = self.parser.parse(tagged_text)
+            tags_reordered = Permuter(tokens).generate_permutations()
+            verbalized_text = self.verbalizer.verbalize(tags_reordered)
+            postprocessed_text = self.post_process(verbalized_text)
+            uppercase_restored = UppercaseRestorer(text, postprocessed_text).run()
+            whitespaces_restored = WhitespaceRestorer(text, uppercase_restored, tokens).run()    
+        
+        except ValueError:
+            if self.debug_mode:
+                raise
+            else:
+                return text
+       
+        else:
+            return whitespaces_restored
+
+    def pre_process(self, text: str):
+        text = text.lower()
+        
+        # Add spaces around punctuation
+        for punct in punctuation:
+            text = text.replace(punct, f" {punct} ")
+        
+        # Remove multiple spaces
+        text = re.sub(r' +', ' ', text)
+        text = text.strip()
+        return text
+
+    def post_process(self, text):
+        text = re.sub(r' +', ' ', text)
+        text = text.strip()
+        logger.debug(f"post_process(): {text}")
+        return text
