a426eb7c99eaad26735daa5b5f74d81d15e44af5
Maciek Stopa <39024197+mstopa@users.noreply.github.com>
2023-06-10 10:43:10 +0200

    Update tests (#14)
    * Test if matrix strategy works
    
    * Test if matrix strategy works
    
    * Temporarily remove docker.yaml
    
    * Fix typo
    
    * Update tests
    
    * Test merging commands
    
    * Fix command
    
    * Put grammars in directories dedicated to cases
    
    * Restore docker pipeline
    
    ---------
    
    Co-authored-by: Maciek Stopa <maciej.stopa@techmo.pl>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
+from pathlib import Path
+
 import pytest
 
-def pytest_addoption(parser, autouse=True):
-    parser.addoption("--save-results", action="store_true", help="Save test results.")
+def pytest_addoption(parser):
+    parser.addoption("--config", type=Path, required=True, help="Path to config file.")
+    parser.addoption("--grammars", type=Path, default=Path("pl_itn/grammars"), help="Path grammars directory.")
 
-@pytest.fixture(scope='session', autouse=True)
-def save_results(request):
-    return request.config.getoption("--save-results")
+@pytest.fixture()
+def config_path(pytestconfig):
+    return Path(pytestconfig.getoption("--config"))
+
+@pytest.fixture()
+def grammars_dir(pytestconfig):
+    return Path(pytestconfig.getoption("--grammars"))
